2da4f5c683ca1976ab6a673a9cdf48eab1204202
dustinpfister <dustin.pfister@gmail.com>
2020-10-21 15:40:15 -0400

    git check

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 #!/usr/bin/env node
+let exec = require('child_process').exec;
 
 let gitFolderCheck = (dir) => {
     return new Promise((resolve, reject) => {
-        resolve();
+
+        let check = exec('git status');
+        check.stdout.on('data', function (data) {
+            resolve(data.toString());
+        });
+        check.stderr.on('data', function (data) {
+            reject(data.toString());
+        });
     });
 };
 
+// git check
 gitFolderCheck(process.cwd())
-
-.then(() => {
-
-    console.log('hello');
-
+.then((data) => {
+    console.log('this is a git');
 })
-
 .catch((e) => {
-
-    console.log(e.message);
-
+    console.log('looks like this might not be a git folder');
 });
