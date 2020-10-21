2b0a679b94e12a99dece43013460324d9c2932d5
dustinpfister <dustin.pfister@gmail.com>
2020-10-21 15:47:40 -0400

    get commit list

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 let exec = require('child_process').exec;
 
-let gitFolderCheck = (dir) => {
+// just check if this is a git folder
+let gitFolderCheck = () => {
     return new Promise((resolve, reject) => {
-
         let check = exec('git status');
         check.stdout.on('data', function (data) {
             resolve(data.toString());
@@ -14,11 +14,38 @@ let gitFolderCheck = (dir) => {
     });
 };
 
+// git log -n 20 --format="%H"
+
+let gitLogCommitList = (dir) => {
+    return new Promise((resolve, reject) => {
+        let list = exec('git log -n 20 --format=\"%H\"'),
+        out = '';
+        list.stdout.on('data', function (data) {
+            //resolve(data.toString());
+            out += data.toString();
+        });
+        list.on('exit', function () {
+            resolve(out);
+
+        });
+        list.stderr.on('data', function (data) {
+            reject(data.toString());
+        });
+    });
+};
+
 // git check
-gitFolderCheck(process.cwd())
-.then((data) => {
-    console.log('this is a git');
-})
+gitFolderCheck()
 .catch((e) => {
     console.log('looks like this might not be a git folder');
+})
+.then((data) => {
+    console.log('this is a git');
+    return gitLogCommitList();
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((e) => {
+    console.log(e);
 });
