b455baec8989a824edd670100d3bc154ea35a106
dustinpfister <dustin.pfister@gmail.com>
2020-10-22 13:03:50 -0400

    makedown.js

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
 #!/usr/bin/env node
 let exec = require('child_process').exec,
-git = require('./lib/git.js');
+git = require('./lib/git.js'),
+markdown = require('./lib/markdown.js');
 
-let fs = require('fs'),
-promisify = require('util').promisify,
-readdir = promisify(fs.readdir);
-
-// check to see if we have any markdown files
-let mdCheckFor = (dir) => {
-    return readdir(dir)
-    .then((files) => {
-        var i = 0;
-        while (i < files.length) {
-            let match = files[i].match(/\.md$/);
-            if (match) {
-                return Promise.resolve('markdown file found');
-            }
-            i += 1;
-        }
-        return Promise.reject('no markdown files found.');
-    });
-};
-
-// git check
-//gitFolderCheck()
 git.folderCheck()
 .catch((e) => {
     console.log(e);
     return Promise.reject('not a git folder');
 })
 .then(() => {
-    return mdCheckFor(process.cwd());
+    return markdown.checkForFiles(process.cwd());
 })
 .catch((e) => {
     console.log(e);
     return Promise.reject('can not check git log');
 })
-
 .then(() => {
-    //return gitLogCommitList(process.cwd(), 10);
-	return git.commitList(process.cwd(), 10);
+    return git.commitList(process.cwd(), 10);
 })
 .then((data) => {
     console.log(data);
